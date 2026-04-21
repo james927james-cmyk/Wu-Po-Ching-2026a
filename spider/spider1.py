31d2840685ae859d7d61efcc2085ff9a5ec10f17
james927james-cmyk <james927james@gmail.com>
2026-04-21 12:04:54 +0800

    Add files via upload

---
+++ spider/spider1.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://wu-po-ching-2026a.vercel.app/about"
+Data = requests.get(url)
+Data.encoding = "utf-8"
+#print(Data.text)
+sp = BeautifulSoup(Data.text, "html.parser")
+result=sp.select("a")
+print(result)
+for item in result:
+	print(item)
