db5a2b9696ed36484edaebc7b4e1a521ef313298
james927james-cmyk <james927james@gmail.com>
2026-05-12 11:48:38 +0800

    5/12 - 03

--- web.py
+++ web.py
@@ -60,6 +60,7 @@ def index():
     link += "<a href=/opendata>113十大肇事路口</a><hr>"
     link += "<a href=/weather>查詢天氣</a><hr>"
     link += "<a href=/rate>本週新片進DB</a><hr>"
+    link += "<a href=/webhook3>本週分級</a><hr>"
     return link
 
 @app.route("/webhook", methods=["POST"])
@@ -331,6 +332,36 @@ def search():
         <br><a href=/>回到首頁</a>
         """
         return html
+
+@app.route("/webhook3", methods=["POST"])
+def webhook3():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req.get("queryResult").get("parameters").get("rate")
+        info = "我是吳柏慶開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
+        db = firestore.client()
+        collection_ref = db.collection("本週新片含分級")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "片名：" + dict["title"] + "\n"
+                result += "介紹：" + dict["hyperlink"] + "\n\n"
+        info += result
+    return make_response(jsonify({"fulfillmentText": info}))
+
+    
+
+    
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
