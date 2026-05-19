2edce47d5be12285a40f600d6639460399672120
james927james-cmyk <james927james@gmail.com>
2026-05-19 20:55:59 +0800

    5/19

--- web.py
+++ web.py
@@ -82,6 +82,20 @@ def webhook():
 
     return make_response(jsonify({"fulfillmentText": info}))
 
+@app.route("/webhook2", methods=["POST"])
+def webhook2():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req.get("queryResult").get("parameters").get("rate")
+        info = "您選擇的電影分級是：" + rate
+    return make_response(jsonify({"fulfillmentText": info}))
+
+
 @app.route("/rate")
 def rate():
     #本週新片
