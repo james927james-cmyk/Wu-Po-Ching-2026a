d1b69c02a62e3de919c3b3601e7231eddcb33390
james927james-cmyk <james927james@gmail.com>
2026-06-02 11:06:07 +0800

    6/2 AI

--- web.py
+++ web.py
@@ -415,6 +415,7 @@ def webhook3():
         info += result
 
     elif (action == "input.unknown"):
+
         instruction_text = (
             "你是一個熱心且知識豐富的專業智慧助理。"
             "對於使用者的提問，請回覆重點的關鍵字，不要重述問題。"
@@ -422,13 +423,13 @@ def webhook3():
 
 
         ai_config = types.GenerateContentConfig(
-            max_output_tokens = 500,
+            max_output_tokens=500, 
             system_instruction=instruction_text
         )
 
 
         response = client.models.generate_content(
-        model='gemini-3.5-flash', 
+        model='gemini-3.1-flash-lite', 
         contents=req["queryResult"]["queryText"],
         config=ai_config,
         )
